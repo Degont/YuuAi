c4358f4be7f8b4c520eb289ef05bee16b58e1828
Dego <16dm1@queensu.ca>
2023-06-26 04:16:50 -0700

    Changed The order of the message to see if an input will work here

--- yuuai-home.py
+++ yuuai-home.py
@@ -5,7 +5,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():  # put application's code here
-    return 'Hello World!'
+    message = input("What am I seeing here ?")
+    return message
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0" , port = 80)
