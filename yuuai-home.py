a00f6c90bb35ab9fe0c80a9988c66fcca7fb962f
Dego <16dm1@queensu.ca>
2023-06-26 05:32:57 -0700

    Changed a variable passed in a fxn to declarative

--- yuuai-home.py
+++ yuuai-home.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 @app.route('/')
 def home():  # put application's code here
     message = "Hello World!"
-    return render_template("yuuai-home.html", message)
+    return render_template("yuuai-home.html", message=message)
 
 @app.route('/second')
 def second_page():
