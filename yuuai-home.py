21046bfbc7c11769b4614a682ac6e18b12d9caf2
Dego <16dm1@queensu.ca>
2023-06-26 05:26:35 -0700

    Added Clickable html element

--- yuuai-home.py
+++ yuuai-home.py
@@ -1,11 +1,16 @@
-from flask import Flask
+from flask import Flask,url_for , render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+def home():  # put application's code here
+    message = "Hello World!"
+    return render_template("yuuai-home.html", message)
+
+@app.route('/second')
+def second_page():
+    return "This is the second page"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0" , port = 80)
